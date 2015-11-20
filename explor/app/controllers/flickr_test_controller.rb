d8ec0ad075a8e0119fab31734176b849eba17e75
Brittany Brassell <brittany.brassell@gmail.com>
2015-11-20 12:09:40 -0800

    yahoo weather changes to where icons show up for forecast

--- explor/app/controllers/flickr_test_controller.rb
+++ explor/app/controllers/flickr_test_controller.rb
@@ -5,32 +5,33 @@ class FlickrTestController < ApplicationController
 		FlickRaw.api_key=ENV["FLICKR_ID"]
 		FlickRaw.shared_secret=ENV["FLICKR_SECRET"]
 	#array to use to pull in backgrounds for results page
-		f_background = []
-		per_page = 7
+		@f_background = []
+		per_page = 1
 		#loop through search to find search query per limits of number, safe search, content safe, landscape only 
 		f_bg_id = flickr.photos.search(:tags => "paris" + "landscape", :per_page => per_page, :content_type => 1, :safe_search => 1).each do |p|
 
 				f_info = flickr.photos.getInfo(:photo_id => p.id)
 				#grab url
 				url = FlickRaw.url_b(f_info)
+				#use for alt text
 				title = f_info.title
 				#push to array
-				f_background.push(url)
+				@f_background.push(url)
 			end
 			#spits out results in console, remove this when we are in production
-		puts f_background
+		puts @f_background
 
-		f_grid = []
+		@f_grid = []
 		f_grid_id = flickr.photos.search(:tags => "paris" + "travel", :per_page => 15, :content_type => 1, :safe_search => 1).each do |p|
 
 				f_info = flickr.photos.getInfo(:photo_id => p.id)
 				#grab url
-				url = FlickRaw.url_b(f_info)
+				url = FlickRaw.url_s(f_info)
 				title = f_info.title
 				#push to array
-				f_grid.push(url, title)
+				@f_grid.push(url, title)
 			end
-		puts f_grid
+		puts @f_grid
 
 
 
