f98d7607cb269400824a7ae473586a67513eb866
abautist <auggie.bautista@gmail.com>
2015-11-19 16:41:15 -0800

    test again

--- explor/app/controllers/main_controller.rb
+++ explor/app/controllers/main_controller.rb
@@ -5,7 +5,20 @@ class MainController < ApplicationController
   end
 
   def results
-  	response = RestClient.get 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='+'singapore'+'&key=AIzaSyAzBnHtU-hZa_cmI1ZUFL2KoLJg3Bbvob8'
+
+  	lat = 47.6062095
+  	lng = -122.3320708
+  	key = ENV['GOOGLE_PLACES_KEY']
+
+  	response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.to_s+","+lng.to_s+"&radius=5000&types=restaurant&key="+key
   	@results = JSON.parse(response)["results"]
+
+  	cafe_response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.to_s+","+lng.to_s+"&radius=5000&types=cafe&key="+key
+  	@cafes = JSON.parse(cafe_response)["results"]
+
+  	bar_response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.to_s+","+lng.to_s+"&radius=5000&types=bar&key="+key
+  	@bars = JSON.parse(bar_response)["results"]
   end
 end
+
+
