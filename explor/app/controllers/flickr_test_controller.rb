7adddce38ad13450e5323b6f25384d10da577448
Brittany Brassell <brittany.brassell@gmail.com>
2015-11-19 17:16:43 -0800

    masonry added

--- explor/app/controllers/flickr_test_controller.rb
+++ explor/app/controllers/flickr_test_controller.rb
@@ -5,21 +5,38 @@ class FlickrTestController < ApplicationController
 		FlickRaw.api_key=ENV["FLICKR_ID"]
 		FlickRaw.shared_secret=ENV["FLICKR_SECRET"]
 	#array to use to pull in backgrounds for results page
-		flickr_images = []
+		f_background = []
 		per_page = 7
 		#loop through search to find search query per limits of number, safe search, content safe, landscape only 
-		f_id = flickr.photos.search(:tags => "paris" + "landscape", :per_page => per_page, :content_type => 1, :safe_search => 1).each do |p|
+		f_bg_id = flickr.photos.search(:tags => "paris" + "landscape", :per_page => per_page, :content_type => 1, :safe_search => 1).each do |p|
+
 				f_info = flickr.photos.getInfo(:photo_id => p.id)
 				#grab url
 				url = FlickRaw.url_b(f_info)
 				title = f_info.title
 				#push to array
-				flickr_images.push(url)
+				f_background.push(url)
 			end
 			#spits out results in console, remove this when we are in production
-			puts flickr_images
+		puts f_background
+
+		f_grid = []
+		f_grid_id = flickr.photos.search(:tags => "paris" + "travel", :per_page => 15, :content_type => 1, :safe_search => 1).each do |p|
+
+				f_info = flickr.photos.getInfo(:photo_id => p.id)
+				#grab url
+				url = FlickRaw.url_b(f_info)
+				title = f_info.title
+				#push to array
+				f_grid.push(url, title)
+			end
+		puts f_grid
+
+
+
 			
-		render json: f_id
+		render json: f_grid_id
+
 
 
 
