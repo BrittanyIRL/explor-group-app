0e934e42386b4bab8cb90ba0053907c1b3d3f301
abautist <auggie.bautista@gmail.com>
2015-11-19 20:20:07 -0800

    trying to grab photo ref

--- explor/app/controllers/main_controller.rb
+++ explor/app/controllers/main_controller.rb
@@ -6,6 +6,8 @@ class MainController < ApplicationController
 
   def results
 
+
+
   	lat = 47.6062095
   	lng = -122.3320708
   	key = ENV['GOOGLE_PLACES_KEY']
@@ -13,11 +15,26 @@ class MainController < ApplicationController
   	response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.to_s+","+lng.to_s+"&radius=5000&types=restaurant&key="+key
   	@results = JSON.parse(response)["results"]
 
+    photo_search = @results["photos"]["photo_reference"]
+
+    if photo_search != nil
+      @photo = photo_search
+    else
+      @photo = "test"
+    end
+
+
   	cafe_response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.to_s+","+lng.to_s+"&radius=5000&types=cafe&key="+key
   	@cafes = JSON.parse(cafe_response)["results"]
 
   	bar_response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.to_s+","+lng.to_s+"&radius=5000&types=bar&key="+key
   	@bars = JSON.parse(bar_response)["results"]
+
+    binding.pry
+
+    # rest_photo_ref = @results["photos"]["photo_reference"]
+    # @photo = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+rest_photo_ref+"&key="+key
+  
   end
 end
 
