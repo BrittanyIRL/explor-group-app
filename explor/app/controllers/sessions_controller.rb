216283b277096cb12f42b522b02984105629628a
abautist <auggie.bautista@gmail.com>
2015-11-21 15:38:32 -0800

    most auth complete

--- explor/app/controllers/sessions_controller.rb
+++ explor/app/controllers/sessions_controller.rb
@@ -3,8 +3,28 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.authenticate login_params['email'], login_params['password']
+
+  	if user
+  		session[:user_id] = user.id
+  		flash[:success] = "#{user.email} is logged in!"
+  		redirect_to root_path #or do we want to redirect to results path?
+  	else
+  		flash[:danger] = "You must be logged in with the correct username and password!"
+  		redirect_to login_path
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	flash[:success] = "You have successfully logged out!"
+  	redirect_to rooth_path
   end
+
+  private
+
+  def login_params
+  	params.require(:user).permit(:email, :password)
+  end
+
 end
