b92097f327f8d048ff13c0911ab918f011c7be9d
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2015-11-23 14:47:41 -0800

    Location read enabled

--- explor/app/controllers/main_controller.rb
+++ explor/app/controllers/main_controller.rb
@@ -7,17 +7,13 @@ class MainController < ApplicationController
   end
   
   def results
-    query = params[:location]['name']
     @location = params[:location]['name']
-    query.gsub!(/\s/,'_')
+    query = @location.gsub(/\s/,'_')
+
+    
+   
     yahoo_prefix = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'
     yahoo_suffix = '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    #yahoo api call, to trigger first - basing google callback on it
-    #response = RestClient.get 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + query + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    #@weather = JSON.parse(response)['query']['results']['channel']['item']
-    #lat = @weather['lat']
-    #lng = @weather['long']
-
 
     @key = "&key=" + ENV['GOOGLE_PLACES_KEY']
     @photo_prefix = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="
@@ -25,9 +21,6 @@ class MainController < ApplicationController
     response_suffix = "&radius=5000&types=restaurant|cafe|bar"
     art_radius_type = "&radius=5000&types=art_gallery|museum"
     club_radius_type = "&radius=5000&types=night_club"
-    #coordinates_ref = lat + "," + lng
-
-
 
     hydra = Typhoeus::Hydra.hydra
     yahoo_weather_request = Typhoeus::Request.new(yahoo_prefix+query+yahoo_suffix)
@@ -38,19 +31,19 @@ class MainController < ApplicationController
       coordinates_ref = lat + "," + lng
 
       @google_restaurants_request = Typhoeus::Request.new(response_prefix+coordinates_ref+response_suffix+@key)
-      @google_art_request = Typhoeus::Request.new(response_prefix+coordinates_ref+art_radius_type+@key)
-      @google_clubs_request = Typhoeus::Request.new(response_prefix+coordinates_ref+club_radius_type+@key)
+     # @google_art_request = Typhoeus::Request.new(response_prefix+coordinates_ref+art_radius_type+@key)
+     # @google_clubs_request = Typhoeus::Request.new(response_prefix+coordinates_ref+club_radius_type+@key)
       hydra.queue @google_restaurants_request
-      hydra.queue @google_art_request
-      hydra.queue @google_clubs_request
+     # hydra.queue @google_art_request
+     # hydra.queue @google_clubs_request
     end
     hydra.queue yahoo_weather_request
     hydra.run # this is a blocking call that returns once all requests are complete
     
 
 
-    @google_art = JSON.parse(@google_art_request.response.body)["results"]
-    @google_clubs = JSON.parse(@google_clubs_request.response.body)["results"]
+    # @google_art = JSON.parse(@google_art_request.response.body)["results"]
+    # @google_clubs = JSON.parse(@google_clubs_request.response.body)["results"]
     @google_restaurants = JSON.parse(@google_restaurants_request.response.body)["results"]
     #puts responses
 
