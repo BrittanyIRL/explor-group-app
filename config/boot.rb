0337765536f268486116d1ccd4a26fa1a094923e
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2015-11-25 00:33:26 -0800

    Tweaked splash styling

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
+RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
